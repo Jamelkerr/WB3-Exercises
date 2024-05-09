12a381f58e906500f84d46b4415f9ed7613e27d5
jamel kerr <jamel.kerr@yahoo.com>
2024-05-09 16:32:44 -0400

    add

---
+++ simple functions.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+
+function favoriteThings(){
+    console.log("jamel");
+    console.log("scarface");
+    console.log("jerusalem");
+
+}
+
+function whyImHere(){
+    console.log("year up will give me a chance at a high paying job");
+
+}
+
+function favoritePlace(){
+    console.log("cali because its alway sunny");
+    
+    
+
+}
+favoriteThings()
+whyImHere()
+favoritePlace()
